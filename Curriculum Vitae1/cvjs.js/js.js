f5f18555f2d921b5ca4e49c42176171b21ac562c
jonatanolivera <127368780+jonatanolivera@users.noreply.github.com>
2023-03-09 22:16:22 -0300

    initial commit

---
+++ Curriculum Vitae1/cvjs.js/js.js
@@ -0,0 +1,35 @@
+
+
+
+ 
+
+
+document.getElementById("boton2").onclick = function () {
+    console.log ("capturar evento click");
+    document.getElementById("demo").innerHTML = "34 años";  
+}
+
+document.getElementById("boton3").onclick = function () {
+    console.log ("capturar evento click");
+    document.getElementById("demo").innerHTML = "10/02/1989";  
+}
+
+document.getElementById("boton4").onclick = function () {
+    console.log ("capturar evento click");
+    document.getElementById("demo").innerHTML = "jonatanolivera89@gmail.com";  
+}
+
+document.getElementById("boton5").onclick = function () {
+    console.log ("capturar evento click");
+    document.getElementById("demo").innerHTML = "1167050734";  
+}
+
+document.getElementById("boton6").onclick = function () {
+    console.log ("capturar evento click");
+    document.getElementById("demo").innerHTML = "Utilización de equipos de diagnostico, inyección electónica, Airbag, ABS, ESP, motor, codificacion de llaves ,configuracion de computadoras automotrices";  
+}
+
+document.getElementById("boton7").onclick = function () {
+    console.log ("capturar evento click");
+    document.getElementById("demo").innerHTML = "Taller Mecanico Delfis:2006-2015 ; Galante Dantonio: 2015-Hasta la fecha"; 
+    }
